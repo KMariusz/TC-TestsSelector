239ff65758698401317f80a7123f44c03e3624cb
KMariusz <kowalski.mariusz.87@gmail.com>
2018-08-16 10:29:20 +0200

    Add files via upload

---
+++ TestsSelector/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests Selector")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mariusz Kowalski")]
+[assembly: AssemblyProduct("Tests Selector")]
+[assembly: AssemblyCopyright(TestsSelector.Settings.Copyright)]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("8e2110ec-deb1-44e1-bac1-e41942855322")]
+[assembly: AssemblyVersion(TestsSelector.Settings.Version_Assembly)]
+[assembly: AssemblyFileVersion(TestsSelector.Settings.Version_Assembly)]
